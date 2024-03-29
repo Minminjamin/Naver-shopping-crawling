13f2c4c958f94744f013286a3d2b14e6218a1650
Minminjamin <0429el@gmail.com>
2024-03-29 15:08:51 +0900

    feature : 초기 셋팅

---
+++ main.py
@@ -0,0 +1,17 @@
+# 라이브러리 호출단
+import requests
+from bs4 import BeautifulSoup
+ 
+import pandas as pd
+from datetime import datetime
+import time
+ 
+# selenium import
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from datetime import datetime, timedelta
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
